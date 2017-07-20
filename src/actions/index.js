3c2ecd4c71d34cdc341a9867ed8a993358f725b7
nick <ncksllvn@gmail.com>
2017-07-20 10:02:11 -0400

    Move to static JSON to get around CORS

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,34 +1,11 @@
+import menu from '../data'
+
 export const RECEIVE_MENU = 'RECEIVE_MENU'
 export const ADD_PIZZA = 'ADD_PIZZA'
 export const REMOVE_PIZZA = 'REMOVE_PIZZA'
 
 export function getMenu(){
-    return function(dispatch, getState){
-
-        const query = `
-            {
-                pizzaSizes {
-                    name
-                    maxToppings
-                    basePrice
-                    toppings {
-                        defaultSelected
-                        topping {
-                            name
-                            price
-                        }
-                    }
-                }
-            }
-        `
-
-        return fetch(`https://core-graphql.dev.waldo.photos/pizza?query=${encodeURIComponent(query)}`)
-
-                    .then(response => response.json())
-
-                    .then(json => dispatch({ type: RECEIVE_MENU, menu: json.data.pizzaSizes }))
-
-    }
+    return { type: RECEIVE_MENU, menu: menu.data.pizzaSizes }
 }
 
 export function addPizza(pizza){
